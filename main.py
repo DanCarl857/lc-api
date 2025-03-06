2d4616d198410681778ad68aff82d131a104b496
Daniel Carlson <dancarl758@gmail.com>
2025-03-06 12:06:58 +0100

    [feature]: add versioning

--- main.py
+++ main.py
@@ -5,24 +5,36 @@ from schemas.user import UserCreate, UserLogin
 from schemas.post import PostCreate
 from dependencies import get_current_user, get_db
 
-app = FastAPI()
+app = FastAPI(root_path="/api")
 
-@app.post("/signup")
+api_v1 = FastAPI()
+
+# Routes
+
+# Signup route
+@api_v1.post("/signup")
 def signup_endpoint(user: UserCreate, db=Depends(get_db)):
     return signup(user, db)
 
-@app.post("/login")
+# Login route
+@api_v1.post("/login")
 def login_endpoint(user: UserLogin, db=Depends(get_db)):
     return login(user, db)
 
-@app.post("/post")
+# Add a post
+@api_v1.post("/post")
 def add_post_endpoint(post: PostCreate, user_id: int = Depends(get_current_user), db=Depends(get_db)):
     return add_post(post, user_id, db)
 
-@app.get("/posts")
+# Get all posts
+@api_v1.get("/posts")
 def get_posts_endpoint(user_id: int = Depends(get_current_user), db=Depends(get_db)):
     return get_posts(user_id, db)
 
-@app.delete("/posts/{post_id}")
+# Delete posts
+@api_v1.delete("/posts/{post_id}")
 def delete_post_endpoint(post_id: int, user_id: int = Depends(get_current_user), db=Depends(get_db)):
     return delete_post(post_id, user_id, db)
+
+# This just adds versioning to the app
+app.mount("/v1", api_v1)
